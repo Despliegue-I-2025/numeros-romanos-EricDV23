7016c19ca1003390b5869e1041c672c44c73a0bf
Eric <ericdanielvelazquez@gmail.com>
2025-11-19 22:13:14 -0300

    Velázquez Eric

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import app from "./server.js";
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
